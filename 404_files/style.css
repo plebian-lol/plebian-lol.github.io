56e0c176a26a2ebcac73070864a030a7d67d6bb5
plebian-lol <editioncds@icloud.com>
2015-12-04 10:47:40 +0100

    music

---
+++ 404_files/style.css
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>404 Not Found</title>
+
+    <link rel="stylesheet" type="text/css" href="scripts/style.css">
+    <script type="text/javascript" src="scripts/countdown.js"></script>
+    <link rel='shortcut icon' type='image/x-icon' href='favicon.ico' />
+</head>
+
+<body>
+    <div class="verticalcenter">
+        <center>
+            <div class="big">Damn, this place is empty.</div>
+            <br>
+            <br>
+            <div class="medium">
+                <div id="countDown"></div>
+            </div>
+
+        </center>
+    </div>
+</body>
+
+</html>
